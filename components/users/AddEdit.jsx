ad2685fcab0b6ee93f3aaf74d7c09f6b4220e2fe
Robert Mill <r.mill@ntlworld.com>
2022-10-23 18:42:47 +0100

    intiail commit 5

--- components/users/AddEdit.jsx
+++ components/users/AddEdit.jsx
@@ -3,8 +3,8 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 
-import { Link } from 'components';
-import { userService, alertService } from 'services';
+import { Link } from '../../components';
+import { userService, alertService } from '../../services';
 
 export { AddEdit };
 
